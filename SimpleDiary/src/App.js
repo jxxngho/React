22daf574784db106ccdcdeff4b293c8052a4e8bf
jxxngho <qkdhfh8@naver.com>
2023-11-21 15:40:40 +0900

    tmp

--- SimpleDiary/src/App.js
+++ SimpleDiary/src/App.js
@@ -5,21 +5,21 @@ import DiaryList from "./Components/DiaryList";
 
 // const dummyList = [
 //   {
-//     id: 1,
+//     id: 11,
 //     author: "이정환 1",
 //     content: "하이 1",
 //     emotion: 1,
 //     created_date: new Date().getTime(),
 //   },
 //   {
-//     id: 2,
+//     id: 22,
 //     author: "아무개 1",
 //     content: "하이 2",
 //     emotion: 5,
 //     created_date: new Date().getTime(),
 //   },
 //   {
-//     id: 3,
+//     id: 33,
 //     author: "홍길동",
 //     content: "하이 3",
 //     emotion: 4,
